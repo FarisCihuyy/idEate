e5e2a57b98d50244a2388d2b840e3fdd449e7272
Faris Sulianto <159701497+FarisCihuyy@users.noreply.github.com>
2025-05-03 17:30:54 +0700

    Update Avatar.jsx

--- src/components/Fragments/Avatar.jsx
+++ src/components/Fragments/Avatar.jsx
@@ -1,5 +1,5 @@
 const Avatar = ({ imgUrl }) => {
-  return <div className={`aspect-square h-full bg-[url(${imgUrl})] bg-contain rounded-full`}></div>;
+  return <div className={`aspect-square h-full bg-[url("${imgUrl}")] bg-contain rounded-full`}></div>;
 };
 
 export default Avatar;
